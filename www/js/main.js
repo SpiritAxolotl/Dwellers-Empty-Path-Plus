a2a57767a15ff713ab62e767113197298e283773
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-09-29 00:53:41 -0600

    quick fixes

--- www/js/main.js
+++ www/js/main.js
@@ -7,13 +7,13 @@ const info = document.getElementById("info");
 const hideinfo = document.getElementById("hideinfo");
 document.addEventListener("DOMContentLoaded", function() {
     div.style.display = "flex";
-    info.style.display = "block";
     PluginManager.setup($plugins);
     hideinfo.addEventListener("click", function(e) {
         info.style.display = "none";
     }, {once: true});
     div.addEventListener("click", function(e) {
         div.style.display = "none";
+        info.style.display = "block";
         SceneManager.run(Scene_Boot);
     }, {once: true});
 }, {once: true});
